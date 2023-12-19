b47b309a147b5b43f5b75abe08136c815110ba5a
Dennis <suwatchai.kaew@gmail.com>
2023-12-19 13:53:25 +0700

    DRY principle

--- src/App.jsx
+++ src/App.jsx
@@ -70,7 +70,7 @@ function ProductSection() {
             />
             <h3>Premium Bluetooth Headphones</h3>
             <p className="product-price">2,400 Baht</p>
-            <Button buttonText="Add to Cart" />
+            <Button />
           </div>
           <div className="product-card">
             <img
@@ -79,7 +79,7 @@ function ProductSection() {
             />
             <h3>Classic Leather Watch</h3>
             <p className="product-price">4,000 Baht</p>
-            <Button buttonText="Add to Cart" />
+            <Button />
           </div>
           <div className="product-card">
             <img
@@ -88,10 +88,7 @@ function ProductSection() {
             />
             <h3>Organic Green Tea</h3>
             <p className="product-price">79.99 Baht</p>
-            <Button
-              buttonText="Add to Cart"
-              buttonStyle="add-to-cart-button-secondary"
-            />
+            <Button buttonStyle="add-to-cart-button-secondary" />
           </div>
         </div>
       </div>
@@ -107,7 +104,10 @@ function Footer() {
   );
 }
 
-function Button({ buttonText, buttonStyle = "add-to-cart-button" }) {
+function Button({
+  buttonText = "Add to Cart",
+  buttonStyle = "add-to-cart-button",
+}) {
   return <button className={buttonStyle}>{buttonText}</button>;
 }
 
